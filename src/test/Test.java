8d24906f9902f015bbf197382ba96d2f1aceee41
kevincezheng <2383406@163.com>
2015-11-25 16:53:13 +0800

    changes log

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import com.zj.utils.StringUtils;
+
+public class Test {
+	public static void main(String[] args) {
+		String str = "kskksk";
+		System.out.println(StringUtils.isEmail(str));
+	}
+}
